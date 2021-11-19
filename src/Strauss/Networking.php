5965876528abc9717cabf89538c00c50cdd51ce2
Swift-Silence <swift.silence2014@gmail.com>
2021-11-19 02:15:02 -0600

    Added function to get remote IP address

--- src/Strauss/Networking.php
+++ src/Strauss/Networking.php
@@ -7,7 +7,16 @@ class Networking
 
     public static function getRemoteIPAddress()
     {
-        
+        $ip = "";
+
+        if (!empty($_SERVER['HTTP_CLIENT_IP']))
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else 
+            $ip = $_SERVER['REMOTE_ADDR'];
+
+        return $ip;
     }
 
 }
